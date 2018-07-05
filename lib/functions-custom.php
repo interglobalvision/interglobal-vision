6f1add7631e48ea02c1756902a5ed73de426ad99
Cas <cas@interglobal.vision>
2018-07-05 16:14:11 -0500

    scaffold project section

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -1,3 +1,18 @@
 <?php
 
 // Custom functions (like special queries, etc)
+function is_videos($files) {
+  foreach ($files as $file) {
+    if (stripos($file, '.mp4') !== false || stripos($file, '.webm') !== false) {
+      return true;
+    }
+  }
+}
+
+function is_images($files) {
+  foreach ($files as $file) {
+    if (stripos($file, '.jpg') !== false || stripos($file, '.png') !== false || stripos($file, '.gif') !== false) {
+      return true;
+    }
+  }
+}
---
+++ partials/project.php
@@ -0,0 +1,74 @@
+<section id="project">
+<?php
+if (have_posts()) {
+  while (have_posts()) {
+    the_post();
+    $project_doc = get_post_meta( get_the_ID(), '_igv_project_documentation', true );
+?>
+
+        <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+          <div class="grid-row">
+            <div class="item-s-12 item-l-6 offset-l-1">
+              <?php the_content(); ?>
+            </div>
+          </div>
+
+<?php
+    // Iterate thru rows
+    foreach($project_doc as $row_key => $row) {
+      // Make row classes (alignment and justify)
+      $row_classes = "{$row['align_items']} {$row['justify_content']}";
+?>
+  <div class="grid-row margin-bottom-basic <?php echo $row_classes; ?>">
+<?php
+      if (!empty($row['caption'])) { // CAPTION
+        $item_classes = "{$row['item_widths'][0]} offset-l-1";
+
+?>
+            <div class="<?php echo $item_classes; ?>">
+              <?php echo apply_filters('the_content', $row['caption']); ?>
+            </div>
+<?php
+      } else if (!empty($row['files'])) { // FILES
+
+        $files = $row['files'];
+
+        // Go thru items in the row
+        foreach($files as $item_key => $items) {
+          $item_classes = !empty($row['item_widths'][$item_key]) ? $row['item_widths'][$item_key] : '';
+
+          if (is_videos($items)) { // VIDEO
+            $videos = $items;
+?>
+            <div class="<?php echo $item_classes; ?>">
+              <?php include(locate_template('partials/video.php')); ?>
+            </div>
+<?php
+          } else if (is_images($items)) { // IMAGE
+            $media_id = array_keys($items)[0];
+?>
+            <div class="<?php echo $item_classes; ?>">
+              <?php echo wp_get_attachment_image($media_id, 'gallery'); ?>
+            </div>
+<?php
+          }
+        }
+      }
+?>
+
+          </div>
+<?php
+    }
+?>
+
+        </article>
+
+<?php
+  }
+} else {
+?>
+        <article class="u-alert grid-item item-s-12"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+<?php
+} ?>
+</section>
+<?php
