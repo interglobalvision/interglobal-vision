2ec19cf7b286e9942874fabeb056fb67ccdffa0a
mikeyrayvon <mikey@interglobal.vision>
2018-07-06 17:45:04 -0500

    style project

--- partials/project.php
+++ partials/project.php
@@ -1,4 +1,6 @@
 <section id="project">
+  <div id="project-wrapper">
+    <div class="container padding-bottom-mid">
 <?php
 if (have_posts()) {
   while (have_posts()) {
@@ -7,8 +9,8 @@ if (have_posts()) {
 ?>
 
         <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-          <div class="grid-row">
-            <div class="item-s-12 item-l-6 offset-l-1 font-size-mid">
+          <div class="grid-row margin-top-extra margin-bottom-basic">
+            <div class="grid-item item-s-12 item-l-6 offset-l-1 font-size-mid">
               <?php the_content(); ?>
             </div>
           </div>
@@ -19,41 +21,54 @@ if (have_posts()) {
       // Make row classes (alignment and justify)
       $row_classes = "{$row['align_items']} {$row['justify_content']}";
 ?>
-          <div class="grid-row margin-bottom-mid <?php echo $row_classes; ?>">
+          <div class="grid-row">
 <?php
       if (!empty($row['caption'])) { // CAPTION
 ?>
-            <div class="item-s-12 item-m-10 item-l-6 offset-l-1 font-size-mid">
+            <div class="item-s-12 item-m-10 item-l-6 offset-l-1 font-size-mid margin-top-basic margin-bottom-basic">
               <?php echo apply_filters('the_content', $row['caption']); ?>
             </div>
 <?php
       } else if (!empty($row['files'])) { // FILES
-
+?>
+            <div class="project-image-row item-s-12 item-m-12 item-l-8 offset-l-2 no-gutter grid-row <?php echo $row_classes; ?>">
+<?php
         $files = $row['files'];
 
         // Go thru items in the row
         foreach($files as $item_key => $items) {
           $item_classes = !empty($row['item_widths'][$item_key]) ? $row['item_widths'][$item_key] : '';
+?>
+              <div class="grid-item margin-top-basic margin-bottom-basic <?php echo $item_classes; ?>">
+<?php
+          if (!empty($row['item_frame'][$item_key])) {
+?>
+                <div class="browser-frame <?php echo $row['item_frame'][$item_key]; ?>">
+<?php
+          }
 
           if (is_videos($items)) { // VIDEO
             $videos = $items;
-?>
-            <div class="<?php echo $item_classes; ?>">
-              <?php include(locate_template('partials/video.php')); ?>
-            </div>
-<?php
+            include(locate_template('partials/video.php'));
           } else if (is_images($items)) { // IMAGE
             $media_id = array_keys($items)[0];
+            echo wp_get_attachment_image($media_id, 'gallery');
+          }
+
+          if (!empty($row['item_frame'][$item_key])) {
 ?>
-            <div class="<?php echo $item_classes; ?>">
-              <?php echo wp_get_attachment_image($media_id, 'gallery'); ?>
-            </div>
+                </div>
 <?php
           }
+?>
+              </div>
+<?php
         }
+?>
+            </div>
+<?php
       }
 ?>
-
           </div>
 <?php
     }
@@ -65,5 +80,7 @@ if (have_posts()) {
   }
 }
 ?>
+    </div>
+  </div>
 </section>
 <?php
