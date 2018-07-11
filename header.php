b481bb9dde985964f6b4f106102a84079629a982
mikeyrayvon <mikey@interglobal.vision>
2018-07-11 16:24:33 -0500

    use custom font color for arrow

--- header.php
+++ header.php
@@ -31,10 +31,40 @@ get_template_part('partials/seo');
   $color_font = get_post_meta($home_page->ID, '_igv_font_color', true);
 
   echo '<style type="text/css">';
-  echo !empty($color_home) ? 'html, #project-close-overlay { background-color: ' . $color_home . '}' : '';
-  echo !empty($color_footer) ? 'footer#footer { background-color: ' . $color_footer . '}' : '';
-  echo !empty($color_project) ? '#project { background-color: ' . $color_project . '}' : '';
-  echo !empty($color_font) ? 'html, body.contact-stuck #footer .contact-item { color: ' . $color_font . '}' : '';
+  if (!empty($color_home)) {
+    echo "
+      html,
+      #project-close-overlay {
+        background-color: $color_home;
+      }
+    ";
+  }
+  if (!empty($color_footer)) {
+    echo "
+      footer#footer {
+        background-color: $color_footer;
+      }
+    ";
+  }
+  if (!empty($color_project)) {
+    echo "
+      #project {
+        background-color: $color_project;
+      }
+    ";
+  }
+  if (!empty($color_font)) {
+    echo "
+      html,
+      body.contact-stuck #footer .contact-item {
+        color: $color_font;
+      }
+
+      .arrow:before {
+        border-top-color: $color_font;
+      }
+    ";
+  }
   echo '</style>';
 ?>
 
